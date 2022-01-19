4613743b0d303aa78985074c94c970f4367fa554
Lizaan Martin <lizaanmartin@gmail.com>
2022-01-19 17:02:23 +0200

    create user interface and start with calculator logic

--- script.js
+++ script.js
@@ -42,3 +42,51 @@ function operate(operator, num1, num2) {
   if (operator === "sqrt") return operation(num1);
   return operation(num1, num2);
 }
+
+function showOutput(event) {
+  output.textContent += event.target.innerText;
+}
+
+function buildExpression(event) {
+  const operator = event.target.innerText;
+  expression.push(Number(output.textContent));
+  expression.push(operator);
+  clearOutput();
+}
+
+function evaluateExpression() {
+  if (output.textContent) expression.push(Number(output.textContent));
+  clearOutput();
+  console.log(expression);
+}
+
+function backSpace() {
+  const text = output.textContent;
+  output.textContent = text.slice(0, -1);
+}
+
+function clearOutput() {
+  output.textContent = "";
+}
+
+const expression = [];
+const output = document.querySelector(".output");
+
+const operandButtons = document.querySelectorAll(".operand-btn");
+operandButtons.forEach((btn) => btn.addEventListener("click", showOutput));
+
+const operatorButtons = document.querySelectorAll(".operator-btn");
+operatorButtons.forEach((btn) =>
+  btn.addEventListener("click", buildExpression)
+);
+
+const evaluateButtons = document.querySelectorAll(".evaluate-btn");
+evaluateButtons.forEach((btn) =>
+  btn.addEventListener("click", evaluateExpression)
+);
+
+const backButton = document.querySelector("#button-back");
+backButton.addEventListener("click", backSpace);
+
+const clearButtton = document.querySelector("#button-clear");
+clearButtton.addEventListener("click", clearOutput);
